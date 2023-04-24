257de8c22be09f7186b7df15e3fc906b1416d182
pmailly <paulma16100@gmail.com>
2023-04-24 19:06:57 +0200

    x

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-wp config create --dbname=test --dbuser=onlineuser --dbpass=password --dbhost=mariadb --allow-root --path=/var/www/wordpress
+cd /var/www/html
 
-wp core install --url=pmailly.42.fr --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com --allow-root --path=/var/www/wordpress
+wp core download --allow-root
+
+wp config create --dbname=test --dbuser=onlineuser --dbpass=password --dbhost=mariadb --allow-root
+
+wp core install --url=pmailly.42.fr --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com --allow-root
 
 /usr/sbin/php-fpm7.3 -F -R
