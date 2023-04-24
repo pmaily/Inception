f172ca1873425506f5860099b741a5d4bfb623a7
pmailly <paulma16100@gmail.com>
2023-04-24 23:01:17 +0200

    check service_healthy

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,10 +2,18 @@
 
 cd /var/www/html
 
+#FILE=/var/www/html/wp-config.php
+#if [ ! -f "$FILE" ]; then
+
+#rm -f wp-config.php
+
 wp core download --allow-root
 
-wp config create --dbname=test --dbuser=onlineuser --dbpass=password --dbhost=mariadb --allow-root
+wp config create --dbname=test --dbuser=onlineuser --dbpass=password --dbhost=mariadb --allow-root --extra-php
 
-wp core install --url=pmailly.42.fr --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com --allow-root
+wp core install --url=https://pmailly.42.fr --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com --allow-root
+
+#fi
+
 
 /usr/sbin/php-fpm7.3 -F -R
