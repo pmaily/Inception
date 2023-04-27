f68aa2505e6a29a6349a47f58d4733faaf161cd2
pmailly <paulma16100@gmail.com>
2023-04-27 22:05:03 +0200

    finish without makefile

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,18 +2,16 @@
 
 cd /var/www/html
 
-#FILE=/var/www/html/wp-config.php
-#if [ ! -f "$FILE" ]; then
-
-#rm -f wp-config.php
-
 wp core download --allow-root
 
-wp config create --dbname=test --dbuser=onlineuser --dbpass=password --dbhost=mariadb --allow-root --extra-php
+wp config create --dbname=wordpress --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb --allow-root --extra-php
 
-wp core install --url=https://pmailly.42.fr --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com --allow-root
+wp core install --url=https://pmailly.42.fr --title=Site_Title --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=your@email.com --allow-root
 
-#fi
+wp user create $WP_NORMAL_USER random@example.com --user_pass=$WP_NORMAL_PASSWORD --role=subscriber --allow-root
 
+wp theme install twentyseventeen --allow-root
+
+wp theme activate twentyseventeen --allow-root
 
 /usr/sbin/php-fpm7.3 -F -R
